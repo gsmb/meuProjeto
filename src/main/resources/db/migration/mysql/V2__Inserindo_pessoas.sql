863b4d1530d45a9d92b093e6df28de076e2313c1
Gabriel Moraes <gabrielbar62@gmail.com>
2018-08-23 16:58:20 -0300

    Segunda versão Banco

---
+++ src/main/resources/db/migration/mysql/V2__Inserindo_pessoas.sql
@@ -0,0 +1,7 @@
+INSERT INTO tabela_pessoa VALUES 
+(’Gabriel', 25, 'M','Rua São Joao da Barra', '29-11-1992', '14755976731’),
+('Jéssica', 27, 'F','Rua 24', '18-04-1991', '12345678998’),
+('Fabian', 32, 'M','Rua 26', '17-05-1987', '32145698711’),
+('Kevyn', 20, 'M','Rua Maisa', '01-07-1999', '85214796321’),
+('Romero', 35, 'M','Rua 56', '01-01-1985', '95175365485’),
+('Maria Joaquina', 85, 'M','Rua São Joao da Barra', '14–09-1945', '96374125413');
