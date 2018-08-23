60190df38f331d44c4b9635ab0bc53e8b3341d29
Gabriel Moraes <gabrielbar62@gmail.com>
2018-08-23 13:17:12 -0300

    Criacao Tabela Pessoa

--- target/classes/db/migration/mysql/V1__Create_tabela_pessoa.sql
+++ target/classes/db/migration/mysql/V1__Create_tabela_pessoa.sql
@@ -1,4 +1,10 @@
 create table tabela_pessoa (
-	ID int not null,
-	NAME varchar(100) not null
+	ID int NOT NULL PRIMARY KEY,
+	NOME varchar(100) NOT NULL,
+	IDADE int NOT NULL,
+	SEXO varchar(1) NOT NULL,
+	ENDERECO varchar(300) NOT NULL,
+	DATA_NASCIMENTO date NOT NULL,
+	CPF varchar(11) NOT NULL
+	
 );
